aa3bfe69b1c088c16d5a355afcc0895cfcadc847
MaldoCarre <38377990+MaldoCarre@users.noreply.github.com>
2019-06-28 03:21:29 -0300

    Add files via upload

--- antec/views.py
+++ antec/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from.forms import formAntecedentes
-
+from core.models import Pacientes 
 # Create your views here.
 
 def form_antecedente(request):
-    
+    antecedente = Pacientes.objects.all()
+    context = {'antec':antecedente}
     if request.method == 'POST':
         form=formAntecedentes(request.POST) #indico metodos POST de la transmicion de mis datos
         if form.is_valid():
@@ -13,6 +14,6 @@ def form_antecedente(request):
         return render(request,"core/home.html")#al ser esto exitoso redirecciono a home
     else:
         form=formAntecedentes#caso contrario atualizo la misma pagina del forulario
-    return render(request,"antec/antecedentes.html",{'form':form}) #renerizo indicando que exitste formulario
+    return render(request,"antec/antecedentes.html",{'form':form},context) #renerizo indicando que exitste formulario
 
 
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth import views as auth_views
 from.import views
 #aca genero las urls que va a tener la app core de nuestro proyecto 
 urlpatterns = [
 
     #path del core
-    path('', views.home, name="home"),
+    path('principal/', views.home, name="home"),
     path('formpaciente/', views.fomPaciente, name="form"),
     path('cargapaciente/', views.cargaPacientes, name="pacienteform"),
-    
-    
+    path('', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
 ]
